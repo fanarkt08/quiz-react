a9d38d6985a792ed6c41b65bb0b9b2cbc2f9a9bf
fanarkt08 <fanarkt08@gmail.com>
2025-05-22 10:43:47 +0200

    change json

--- src/components/FilterQuiz.jsx
+++ src/components/FilterQuiz.jsx
@@ -4,7 +4,7 @@ import { Form } from 'react-bootstrap';
 function FilterQuiz({ onCategoryChange }) {
   return (
     <Form.Select className="my-3" onChange={e => onCategoryChange(e.target.value)}>
-      <option value="All">Toutes les catégories</option>
+      <option value="All">Toutes</option>
       <option value="Math">Math</option>
       <option value="Science">Science</option>
       <option value="Histoire">Histoire</option>
